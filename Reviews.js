a76ea68d1160fc991da48a2c95c55648a76a3597
Justin Hoang <justin.lhoang3@gmail.com>
2024-03-30 17:09:13 -0600

    syntax error fix in reviews collection

--- Reviews.js
+++ Reviews.js
@@ -5,7 +5,7 @@ mongoose.connect(process.env.DB);
 
 // Movie schema
 var ReviewSchema = new Schema({
-    moveId: { type: Schema.Types.ObjectID, ref: 'Movie' },
+    movieId: { type: Schema.Types.ObjectID, ref: 'Movie' },
     username: String,
     review: String,
     rating: { type: Number, min: 0, max: 5 }
